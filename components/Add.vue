b414f0882d230879371b694d9addc69d65f5169e
72pranjal <pranjal.gupta@vidyamantra.com>
2022-08-24 17:51:41 +0530

    adding edit functionality

--- components/Add.vue
+++ components/Add.vue
@@ -1,50 +1,233 @@
 <template>
-  <div class=" bg-zinc-300 ">
+  <div class="bg-zinc-300">
     <div class="pt-20">
       <transition name="slide-fade">
-        <p v-if="line1Show" class="text-4xl ml-24 p-4 cursor-pointer hover:scale-105 duration-1000 hover:text-indigo-700 text-amber-600 ">=>You Can Add</p>
+        <p
+          v-if="line1Show"
+          class="
+            text-4xl
+            ml-24
+            p-4
+            cursor-pointer
+            hover:scale-105
+            duration-1000
+            hover:text-indigo-700
+            text-amber-600
+          "
+        >
+          =>You Can Add
+        </p>
       </transition>
 
       <transition name="slide-fade">
-        <p v-if="line2Show" class="text-4xl ml-44 cursor-pointer hover:scale-105 duration-1000 hover:text-stone-50 text-slate-500">=>Your Items Hear</p>
+        <p
+          v-if="line2Show"
+          class="
+            text-4xl
+            ml-44
+            cursor-pointer
+            hover:scale-105
+            duration-1000
+            hover:text-stone-50
+            text-slate-500
+          "
+        >
+          =>Your Items Hear
+        </p>
       </transition>
       <transition name="slide-fade">
-        <p v-if="line3Show" class="text-4xl ml-64 cursor-pointer hover:scale-105 duration-1000 hover:text-red-600 text-sky-600 p-4">
+        <p
+          v-if="line3Show"
+          class="
+            text-4xl
+            ml-64
+            cursor-pointer
+            hover:scale-105
+            duration-1000
+            hover:text-red-600
+            text-sky-600
+            p-4
+          "
+        >
           =>This Is A Type Of Todo List
         </p>
       </transition>
       <transition name="slide-fade">
-        <p v-if="line4Show" class="text-4xl hover:text-pink-800   hover:scale-105 duration-1000 cursor-pointer text-emerald-500 ml-96">
+        <p
+          v-if="line4Show"
+          class="
+            text-4xl
+            hover:text-pink-800 hover:scale-105
+            duration-1000
+            cursor-pointer
+            text-emerald-500
+            ml-96
+          "
+        >
           =>Todo List Is Use For Store Data
         </p>
       </transition>
     </div>
-     <div class="mt-12 bg-gray-600 h-auto w-6/12 text-center rounded-2xl border-8 mx-auto">
+     <form @submit.prevent="addTodo " autocomplete="off">
+      <div
+        class="
+          mt-12
+          bg-gray-600
+          h-auto
+          w-6/12
+          text-center
+          rounded-2xl
+          border-8
+          mx-auto
+        "
+      >
         <div class="p-6">
-            <label class="text-2xl mr-56 text-neutral-300 " for="name">Your Item Name...</label><br>
-            <input class="h-10 w-3/4 p-4 focus:bg-gray-100 border-none bg-orange-100 my-4 rounded-md"  type="text" id="name" placeholder="enter your items names">
+          <label class="text-2xl mr-56 text-neutral-300" for="name"
+            >Your Item Name...</label
+          ><br />
+          <input
+            class="
+              h-10
+              w-3/4
+              p-4
+              focus:bg-gray-100
+              border-none
+              bg-orange-100
+              my-4
+              rounded-md
+            "
+            type="text"
+            id="name"
+            placeholder="enter your items names"
+            v-model="todoName"
+          />
         </div>
-        <div class="text-2xl text-neutral-300"><p>Choose Category of Your Item</p></div>
-        <div class="flex my-10  mx-auto">
-             <div class=" bg-orange-100 h-28 ml-24  rounded-2xl p-12 items-center">
+        <div class="text-2xl text-neutral-300">
+          <p>Choose Category of Your Item</p>
+        </div>
+        <div class="flex my-10 mx-auto">
+          <div class="bg-orange-100 h-28 ml-24 rounded-2xl p-12 items-center">
             <label class="text-x" for="b">Business</label>
-            <input type="radio" id="b" name="category" value="Business">
-        </div>
-         <div class=" bg-orange-100 h-28 ml-16 rounded-2xl p-12">
+            <input
+              type="radio"
+              id="b"
+              name="category"
+              v-model="category"
+              value="Business"
+            />
+          </div>
+          <div class="bg-orange-100 h-28 ml-16 rounded-2xl p-12">
             <label for="p">Personal</label>
-            <input type="radio" id="p" name="category" value="Personal">
+            <input
+              type="radio"
+              id="p"
+              name="category"
+             v-model="category"
+             value="Personal"
+            />
+          </div>
         </div>
+        <div
+          class="
+            mb-10
+            mt-16
+            rounded-xl
+            cursor-pointer
+            hover:bg-blue-400
+            bg-orange-100
+            h-10
+            w-2/4
+            mx-auto
+            items-center
+          "
+        >
+          <button class="text-center p-2">Add Items</button>
         </div>
-        <div class="mb-10 mt-16 rounded-xl cursor-pointer  hover:bg-blue-400  bg-orange-100 h-10 w-2/4 mx-auto items-center">
-            <button class="text-center p-2">Add Items</button>
+      </div>
+      <teleport to="body">
+        <div v-if="checkBlank" class="modal">
+          <p>please Choose name of your todo</p>
+          <p>and category</p>
+          <button @click="checkBlank = false">Close</button>
         </div>
+      </teleport>
+    </form>
+     <AddChild :totaldata="totalSort" @edit="editHandler" />
   </div>
-  </div>
-
  
 </template>
 
-<script setup>
+<script setup lang="ts">
+import func from "~~/vue-temp/vue-editor-bridge";
+
+enum radioType {
+  BUSINESS = "Business",
+  PERSONAL = "Personal",
+}
+
+// const nameHandler = (event) => {
+//   todoName.value = event.target.value;
+  
+//   console.log(todoName.value);
+// }
+// const categoryHandeler=(event)=>{
+// category.value=event.target.value
+// }
+
+const isEditMode = ref(false);
+const i = ref(null);
+
+const editHandler = (index:number)=>{
+  console.log('hello')
+  isEditMode.value = true;
+   i.value = index;
+  todoName.value = totalSort[i].name;
+  console.log(totalSort[i].name)
+  console.log(todoName.value)
+
+  category.value=totalSort[i].todoCategory
+ 
+  console.log(index)
+}
+
+
+interface typetodo {
+  name: string;
+  todoCategory: radioType;
+}
+const totalTodo = ref<typetodo[]>([]);
+const todoName = ref<string>('');
+const category = ref<radioType>(null);
+const checkBlank = ref<boolean>(false);
+
+const addTodo = () => {
+  // if (todoName.value.trim() === "" || category.value === null) {
+  //   return (checkBlank.value = true);
+  // }
+  totalTodo.value.push({
+    name: todoName.value,
+    todoCategory: category.value,
+  });
+  todoName.value = "";
+  category.value = null;
+  console.log(totalTodo.value);
+};
+watch(totalTodo,(newVal)=>{
+  localStorage.setItem("totalTodo",JSON.stringify(newVal));
+},
+{deep:true}
+);
+
+onMounted(()=>{
+  totalTodo.value=JSON.parse(localStorage.getItem('totalTodo')) ||[];
+})
+
+const totalSort = computed(() =>
+  totalTodo.value.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  })
+);
+console.log(totalSort.value)
 const line1Show = ref(false);
 onMounted(() => {
   setTimeout(() => {
@@ -87,5 +270,19 @@ onMounted(() => {
 .slide-fade-leave-to {
   transform: translateX(20px);
   opacity: 0;
+}
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+  background-color: red;
+  color: white;
+  font-weight: 800;
+  text-align: center;
+  font-size: 20px;
+  border: none;
 }
 </style>
---
+++ components/AddChild.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>S.NO.</th>
+          <th>Todo Name</th>
+          <th>Todo Category</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(items, index) in props.totaldata" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ items.name }}</td>
+          <td>{{ items.todoCategory }}</td>
+          <td>
+            <button @click="remove(items,index)">Delete</button>/
+            <button @click="emit('edit',index)">Edit</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(["totaldata"]);
+const emit = defineEmits(['edit']);
+
+const remove = (items:Object,index:number) => {
+  props.totaldata.splice(index,1);
+};
+
+</script>
